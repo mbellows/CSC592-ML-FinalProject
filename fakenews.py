728322fd5552c77799aa1d6a825d6cf82261f1c2
sarmenti <sarmenti@my.uri.edu>
2017-12-04 11:32:37 -0500

    Updated fake_news file

--- fakenews.py
+++ fakenews.py
@@ -1,9 +1,33 @@
-import gensim
+# Receive a Runtime Warning but it is only a warning that can be ignored and notebook continues to run without errors
+import tensorflow as tf
+import numpy as np
+import math
+import timeit
+import matplotlib.pyplot as plt
+%matplotlib inline
 
-# Load Google's pre-trained Word2Vec model.
-model = gensim.models.KeyedVectors.load_word2vec_format('./GoogleNews-vectors-negative300.bin', binary=True)  
+def get_embeddings():
+    """
+    Load the CIFAR-10 dataset from disk and perform preprocessing to prepare
+    it for the two-layer neural net classifier. These are the same steps as
+    we used for the SVM, but condensed to a single function.  
+    """
+    # Load the raw embedding data
+    X_train = np.load('embedding_output.npy')
+    
+    y_train = np.load('label_output.npy') 
+    
+    batch = X_train.shape[0]
+    sentence_size = X_train.shape[1]
+    embed_size = X_train.shape[2]
+    
+    X_train = tf.reshape(X_train, shape=[batch, 1, sentence_size, embed_size])
 
-print(model['word'])
-	
-print(model['word'].shape)
+    #return X_train, y_train
+    return X_train, y_train
 
+
+# Invoke the above function to get our data.
+X_train, y_train = get_embeddings()
+print('Train data shape: ', X_train.shape)
+print('Train labels shape: ', y_train.shape)
