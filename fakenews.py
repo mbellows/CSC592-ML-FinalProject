3b64d3f655e60ce81040cec316929140d34cd600
sarmenti <sarmenti@my.uri.edu>
2017-12-07 13:26:55 -0500

    Update fakenews.py

--- fakenews.py
+++ fakenews.py
@@ -3,13 +3,19 @@ import tensorflow as tf
 import numpy as np
 import math
 import timeit
-import matplotlib.pyplot as plt
 import theano
+
+from matplotlib import pyplot as plt
+from matplotlib.pyplot import imshow
+%matplotlib inline
 
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation, Flatten
 from keras.layers import Conv2D, MaxPooling2D
 from keras.utils import np_utils
+
+from keras.utils import plot_model
+import pydot
 
 def get_embeddings():
     """
@@ -17,19 +23,18 @@ def get_embeddings():
     it for the two-layer neural net classifier. These are the same steps as
     we used for the SVM, but condensed to a single function.  
     """
-    folder = '/mnt/data/playground/temp-project-michael/'
     # Load the raw embedding data
-    X_train = np.load(folder+'train_embeddings.npy')
+    X_train = np.load('./train_embeddings.npy')
     
-    y_train = np.load(folder+'train_labels.npy')
+    y_train = np.load('./train_labels.npy')
     
-    X_valid = np.load(folder+'valid_embeddings.npy')
+    X_valid = np.load('./valid_embeddings.npy')
     
-    y_valid = np.load(folder+'valid_labels.npy')
+    y_valid = np.load('./valid_labels.npy')
     
-    X_test = np.load(folder+'test_embeddings.npy')
+    X_test = np.load('./test_embeddings.npy')
     
-    y_test = np.load(folder+'test_labels.npy')
+    y_test = np.load('./test_labels.npy')
 
     #return X_train, y_train
     return X_train, y_train, X_valid, y_valid, X_test, y_test
@@ -53,17 +58,9 @@ embed_size = X_train.shape[2]
 
 X_train = X_train.reshape((batch,1,sentence_size,embed_size))
 X_valid = X_valid.reshape((X_valid.shape[0],1,sentence_size,embed_size))
-print (X_train.shape)
-
-
-X_train = X_train.astype('float32')
-print (X_train.shape)
-print (X_train.dtype)
 
 model = Sequential()
 model.add(Conv2D(128, (2, 2), activation='relu', input_shape=(1,sentence_size,embed_size), dim_ordering='th'))
-print (model.output_shape)
-
 
 model.add(Conv2D(128, (3, 3), activation='relu'))
 model.add(Conv2D(128, (4, 4), activation='relu'))
@@ -73,8 +70,6 @@ model.add(MaxPooling2D(pool_size=(2,2)))
 
 model.add(Dropout(0.8))
 
-print (model.output_shape)
-
 model.add(Flatten())
 model.add(Dense(6, activation='softmax'))
 
@@ -82,5 +77,32 @@ model.compile(loss='categorical_crossentropy',
               optimizer='sgd',
               metrics=['accuracy'])
 
-model.fit(X_train, y_train, 
+# Create graphs of our model (one without the shapes and one with the shapes)
+plot_model(model, to_file='model.png')
+plot_model(model, to_file='model_with_shapes.png', show_shapes=True)
+
+# Train the model
+history = model.fit(X_train, y_train, 
           batch_size=64, nb_epoch=10, verbose=1,validation_data=(X_valid,y_valid))
+
+# Plot the model
+# list all data in history
+print(history.history.keys())
+
+# summarize history for accuracy
+plt.plot(history.history['acc'])
+plt.plot(history.history['val_acc'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
+
+# summarize history for loss
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
